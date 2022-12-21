12678daa6c076b9b798e708afa7007ba85cefa17
Aleksandr Pavlov <pavlovaleksandr@ukr.net>
2022-12-21 19:35:19 +0200

    render
    render the necessary products

--- src/modules/Products/Products.jsx
+++ src/modules/Products/Products.jsx
@@ -1,13 +1,17 @@
 import "./products.scss";
 
-const Products = (parentCategories, categories) => {
-  console.log(parentCategories);
-  console.log(categories);
-  return (
-    <div className="products">
-      <h1>Products</h1>
+const Products = ({ getCurentCategories }) => {
+  console.log(getCurentCategories);
+  const elements = getCurentCategories.map(({ id, name, article }) => (
+    <div key={id} className="card">
+      <div className="card-body">
+        <h5 className="card-title">{name}</h5>
+        <p className="card-text">{article}</p>
+      </div>
     </div>
-  );
+  ));
+
+  return <div className="container wrapper">{elements}</div>;
 };
 
 export default Products;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 
 import Categories from "../../modules/Categories/Categories";
-// import Products from "../../modules/Products/Products";
+import Products from "../../modules/Products/Products";
 
 import getProduction from "../../services/API";
 
@@ -10,34 +10,33 @@ import "./homePage.scss";
 const HomePage = () => {
   const [categories, setCategories] = useState("");
   const [parentCategories, setParentCategories] = useState([]);
-  const [curentCategory, setCurentCategory] = useState([]);
 
   function handleCategories(id) {
     setCategories(id);
   }
 
+  const getCurentCategories = parentCategories.filter((item) => {
+    return Number(item.parent) === categories;
+  });
+
+  // console.log(getCurentCategories);
+
   useEffect(() => {
     try {
       getProduction().then((data) => {
         setParentCategories(data.data.products);
-        if (categories) {
-          setCurentCategory(
-            data.data.products.filter((item) => item.parent === categories)
-          );
-        }
       });
     } catch (error) {
       console.log(error);
     }
   }, [categories]);
 
-  console.log(parentCategories);
-  console.log(curentCategory);
+  // console.log(parentCategories);
 
   return (
     <div className="container">
       <Categories handleCategories={handleCategories} />
-      {/* <Products parentCategories={parentCategories} categories={categories} /> */}
+      <Products getCurentCategories={getCurentCategories} />
     </div>
   );
 };
